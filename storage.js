f22ba3e0bcd2abeee7f6fc452bfb4f97e50dad4f
mdroberts74 <mikedroberts88@gmail.com>
2018-06-04 15:03:31 -0500

    I am creating a database of music gear, I added the objects

--- storage.js
+++ storage.js
@@ -1,33 +1,56 @@
 // Define the database as an object
-const HomeInventoryDatabase = {}
+const gearCollectionInventory = {}
 
 // Define the arrays (a.k.a. tables) in the database to store each type of items
-HomeInventoryDatabase.furniture = []
-HomeInventoryDatabase.crafts = []
-HomeInventoryDatabase.electronics = []
+gearCollectionInventory.guitars = []
+gearCollectionInventory.amps = []
+gearCollectionInventory.pedals = []
 
 
-// The ink well is a craft data item
-const vintageInkwell = {
-    name: "Vintage Ink Well",
-    location: "Writing desk",
-    description: "I enjoy this inkwell because it belonged to my grandfather and holds enough ink to survive weeks of writing."
+// guitars data item
+const fenderTelecaster = {
+    name: "Fender Telecaster",
+    serialNumber: "USA334344334",
+    description: "This guitar was purchased to add a guitar with wide range humbuckers to my collection",
+}
+const gibsonLesPaul = {
+    name: "Fender Telecaster",
+    serialNumber: "USA334344334",
+    description: "Guitar purchased from Hollywood Guitar Center used.",
+}
+const Martin = {
+    name: "Fender Telecaster",
+    serialNumber: "USA33ddf445444",
+    description: "This is my favoirte accoustic guitar",
+}
+const Rickenbacker350 = {
+    name: "Fender Telecaster",
+    serialNumber: "USA334344334",
+    description: "This guitar is a full scale version of the 325 model",
 }
 
-// The writing desk is a furniture data item
-const writingDesk = {
-    name: "Shaker Writing Desk",
-    location: "Bedroom",
+// amplifer data item
+const fenderDeluxeReverb = {
+        name: "Fender 65 Deluxe Reverb",
+        serialNumber: "XZXKDFKEED",
+        description: "This is an amp that is my go to amp!"
+    }
+    //pedals
+const tcElectronicAlterEgo = {
+    name: "TC Electronics Alter Ego",
+    serialNumber: "USAGKGKDKDKGKD",
     description: "This antique desk is special because I found and purchased it with my wife at an Ohio Amish auction."
 }
+
 
 
 // Adding Data Items to the Database
 // Next, place each object that you have defined into the appropriate database table.I'm going to add the two items I created above into the appropriate tables. Remember, I'm using the term table because we're building a database, but the collections are actually arrays, so I can use the push() method.
 
 
-HomeInventoryDatabase.crafts.push(vintageInkwell)
-HomeInventoryDatabase.furniture.push(writingDesk)
+gearCollectionInventory.guitars.push(guitars, fenderTelecaster, gibsonLesPaul, martin, rickenbacker350)
+gearCollectionInventory.amps.push(amplifiers, fenderDeluxeReverb)
+gearCollectionInventory.pedals.push(pedals, tcElectronicAlterEgo)
 
 // As your applications become more complex and you start accepting user input, you will need to change your database and then immediately save it to local storage.It only takes two lines of code to accomplish this, but we want to keep our code DRY, and never write the same line of code twice.
 
@@ -70,15 +93,15 @@ constc loadDatabase = function(localStorageKey) {
 /*
     Initialize the database in memory as an object
 */
-const HomeInventoryDatabase = {}
+const gearCollectionInventory = {}
 
 /*
     Initilize all of the tables (i.e. arrays) that you want to
     use in your database.
 */
-HomeInventoryDatabase.furniture = []
-HomeInventoryDatabase.crafts = []
-HomeInventoryDatabase.electronics = []
+gearCollectionInventory.furniture = []
+gearCollectionInventory.crafts = []
+gearCollectionInventory.electronics = []
 
 /*
     Time to create some data that will inserted into the database
@@ -99,8 +122,8 @@ const writingDesk = {
 }
 
 // Add the data to the appropriate tables
-HomeInventoryDatabase.crafts.push(vintageInkwell)
-HomeInventoryDatabase.furniture.push(writingDesk)
+gearCollectionInventory.crafts.push(vintageInkwell)
+gearCollectionInventory.furniture.push(writingDesk)
 
 // Persist the database to localStorage
-saveDatabase(HomeInventoryDatabase, "HomeInventory")
+saveDatabase(gearCollectionInventory, "HomeInventory")
